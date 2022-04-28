58be2b09975d51411f8f04ff7762ac15be717a4f
gerth <gerth1805@gmail.com>
2022-04-28 12:39:46 +0200

    Better package loading

--- libraries.R
+++ libraries.R
@@ -1,38 +1,6 @@
-# all required libraries can be installed with 
-# install.packages(c( "colourpicker",
-#                     "distory",
-#                     "data.table",
-#                     "doParallel",
-#                     "dplyr",
-#                     "DT",
-#                     "foreach",
-#                     "gridExtra",
-#                     "markdown",
-#                     "phytools",
-#                     "shinycssloaders",
-#                     "shinyjs",
-#                     "shinythemes",
-#                     "shinyWidgets"), 
-#                  dependencies = TRUE)
-
-
 # load libraries
-library(shiny, warn.conflicts = FALSE)
-library(colourpicker, warn.conflicts = FALSE)
-library(coda, warn.conflicts = FALSE)
-library(data.table, warn.conflicts = FALSE)
-library(distory, warn.conflicts = FALSE)
-library(doParallel, warn.conflicts = FALSE)
-library(DT, warn.conflicts = FALSE)
-library(dplyr, warn.conflicts = FALSE)
-library(ggplot2, warn.conflicts = FALSE)
-library(foreach, warn.conflicts = FALSE)
-library(gridExtra, warn.conflicts = FALSE)
-library(markdown, warn.conflicts = FALSE)
-library(phytools, warn.conflicts = FALSE)
-library(shinycssloaders, warn.conflicts = FALSE)
-library(shinyjs, warn.conflicts = FALSE, quietly = TRUE)
-library(shinythemes, warn.conflicts = FALSE)
-library(shinyWidgets, warn.conflicts = FALSE)
-library(stringr, warn.conflicts = FALSE)
-library(tidyr, warn.conflicts = FALSE)
+packages <- c("shiny", "colourpicker", "coda", "data.table", "distory", 
+              "doParallel", "DT", "dplyr", "ggplot2", "foreach", "gridExtra", 
+              "markdown", "phytools", "shinycssloaders", "shinyjs", "shinythemes",
+              "shinyWidgets", "stringr", "tidyr")
+invisible(lapply(packages, library, character.only = TRUE))
